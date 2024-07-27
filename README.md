1b84ef1fd881d0f3ca8f46c1434b2c34c15d7302
epicseven-cup <jackylin616@gmail.com>
2024-07-27 08:37:27 -0400

    syunc

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # bubble
 network traffic cli tool written in rust to help debug web application problems
+
+
+# Note for myself
+
+This is trying to mimic a sidecar
+
+# Goal
+
+- check health of the paths
+- runs unit test
+- schduled task
--- bubble-cli/src/main.rs
+++ bubble-cli/src/main.rs
@@ -1,13 +1,12 @@
 use std::env;
+use std::path;
 use std::process;
-
 
 const ARG_SIZE:usize = 2;
 
 struct Config {
 	url: String
 }
-
 
 impl Config {
 	fn build(args: &[String]) -> Result<Config, &'static str>{
@@ -19,6 +18,13 @@ impl Config {
 	}
 }
 
+fn _parse_path_yml() -> Vec<String> {
+	let mut paths:Vec<String> = Vec::new();
+	let contents = fs::read_to_string("config/paths.yml").expect("Fail to read path.yml");
+	
+	return paths; 
+}
+
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	// The fix size of the args are: ###
@@ -27,6 +33,18 @@ fn main() {
 		process::exit(1);
 	});
 	println!("Hello, world!");
+
+	// This is a test selection where I will check the inputs
+	let url:String = config.url;
+	println!("The url is {url}");
+
+	// Parse path.yml for health check
+	let paths:Vec<String> = Vec::new();
+	loop {
+		// Do stuff
+		println!("Running");
+
+	}
 }
 
 
